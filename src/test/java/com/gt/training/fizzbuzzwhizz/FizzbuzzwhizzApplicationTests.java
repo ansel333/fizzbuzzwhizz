d318ac10889ef1aa20f0910ca6ec0ec724c69e75
ansel <ansel0926@gmail.com>
2020-02-23 15:06:32 +0800

    Initial commit

---
+++ src/test/java/com/gt/training/fizzbuzzwhizz/FizzbuzzwhizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gt.training.fizzbuzzwhizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzwhizzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
